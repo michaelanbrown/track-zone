2979c2fb40c13097d1cdbcf31d92c1d322d48ae4
Michaela Brown <m_brownbrown@yahoo.com>
2023-04-11 20:26:03 -0700

    coach validations added

--- app/models/coach.rb
+++ app/models/coach.rb
@@ -1,4 +1,7 @@
 class Coach < ApplicationRecord
     has_many :runners
     has_many :events, through: :runners
+
+    validates :full_name, presence: true, uniqueness: true
+    validates :age, numericality: { greater_than_or_equal_to: 18 }
 end
