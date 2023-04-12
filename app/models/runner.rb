3699092eeca43f97c516a985b61c0d999e1261af
Michaela Brown <m_brownbrown@yahoo.com>
2023-04-11 20:32:33 -0700

    runner validations added

--- app/models/runner.rb
+++ app/models/runner.rb
@@ -1,4 +1,12 @@
 class Runner < ApplicationRecord
   belongs_to :coach
   belongs_to :event
+
+  validates :full_name, presence: true, uniqueness: true
+  validates :age, numericality: { greater_than_or_equal_to: 13 }
+  validates :coach_id, presence: true
+  validates :event_id, presence: true
+  validates :username, presence: true, uniqueness: true
+
+  has_secure_password
 end
