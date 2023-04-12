44194f16892fb60bfb36ad022af0ad39d84cbeb1
Michaela Brown <m_brownbrown@yahoo.com>
2023-04-11 20:30:16 -0700

    event validations added

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ApplicationRecord
     has_many :runners
     has_many :coaches, through: :runners
+
+    validates :name, presence: true
+    validates :distance, numericality: { greater_than: 0 }
+    validates :unit_of_measurement, presence: true, uniqueness: true, inclusion: { in: %w(km mi m) }
 end
