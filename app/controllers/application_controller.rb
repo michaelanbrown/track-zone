110f9868c32d633e0793994d814d4c0bf6c8549e
Michaela Brown <m_brownbrown@yahoo.com>
2023-04-12 18:36:25 -0700

    login page built out but errors not displaying

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
   
-  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
 
-  def render_unprocessable_entity(invalid)
-      render json: {errors: invalid.record.errors}, status: :unprocessable_entity
-  end 
+  def render_unprocessable_entity_response(exception)
+    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+  end
 
-   def render_not_found(error)
-      render json: {errors: {error.model => "Not Found"}}, status: :not_found
-  end 
+  def render_not_found_response
+    render json: { error: "Activity not found" }, status: :not_found
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :coaches, only: [:index, :create]
 
   post "/login", to: "sessions#create" 
-  delete '/logout', to: "sessions#destroy"
+  delete "/logout", to: "sessions#destroy"
 
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
