74cb16a21141e8f3b914f36b74e912f891c30666
Michaela Brown <m_brownbrown@yahoo.com>
2023-04-12 17:46:04 -0700

    event routes are only index and create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,17 @@
 class EventsController < ApplicationController
+
+    def index 
+        render json: Event.all, status: :ok
+    end
+
+    def create
+        event = Event.create!(event_params)
+        render json: event, status: :created
+    end
+
+    private
+    
+    def event_params
+        params.permit(:full_name, :age, :photo)
+    end 
 end
