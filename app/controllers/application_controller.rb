86ab30df8d319a08e7f633b33afa39c5e42489fd
Michaela Brown <m_brownbrown@yahoo.com>
2023-04-14 17:18:12 -0700

    added @current_user to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,9 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id]) # memoization
+  end
 
   def render_unprocessable_entity_response(exception)
     render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
