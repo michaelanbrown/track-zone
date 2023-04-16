6440853cecc84edf9f4b4ed9ad8eed9cd3413ac2
Michaela Brown <m_brownbrown@yahoo.com>
2023-04-15 19:44:02 -0700

    signup fetch request

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,12 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useContext} from 'react'
+import { useNavigate } from 'react-router-dom';
 import './App.css';
+import { UserContext } from '../context/Runner';
 
 function Signup() {
+    const { currentUser, setCurrentUser } = useContext(UserContext);
+    const [errors, setErrors] = useState([])
     const [formData, setFormData] = useState({
         name:'',
+        username: '',
         email:'',
-        password:''
+        password:'',
+        age: '',
+        photo: '',
+        coach: '',
+        event: ''
     })
+    const {name, username, email, password, age, photo, coach, event} = formData
+    const navigate = useNavigate();
+
+    function onSubmit(e){
+        e.preventDefault()
+        const user = {
+            name,
+            username,
+            email,
+            password,
+            age,
+            photo,
+            coach,
+            event
+        }   
+        fetch(`/runners`,{
+          method:'POST',
+          headers:{'Content-Type': 'application/json'},
+          body:JSON.stringify(user)
+        })
+        .then(res => {
+            if(res.ok){
+                res.json().then(user => {
+                    setCurrentUser(user)
+                    navigate(`/runners/${user.id}`)
+                })
+            }else {
+                res.json().then(json => setErrors(Object.entries(json.errors)))
+            }
+        })
+       
+    }
 
         return (
             <div>Signup</div>
