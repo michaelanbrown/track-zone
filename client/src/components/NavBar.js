0cd5a3837ca5ccce8cd30e0bedd4f30378469dd7
Michaela Brown <m_brownbrown@yahoo.com>
2023-04-17 17:30:29 -0700

    styling changes to navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -23,14 +23,16 @@ export default function NavBar ()  {
             <NavLink className="inactive" activeclassname="active" to="/">Welcome</NavLink>
             { currentUser ? null :<br />}
             { currentUser ? null : <NavLink className="inactive" activeclassname="active" to="/signup">Signup</NavLink>}
-            <br></br>
-            { currentUser ? <button className="bttn" onClick={handleLogOut}>Logout</button> : <NavLink className="inactive" activeclassname="active" to="/login">Login</NavLink>}
+            { currentUser ? null :<br />}
+            { currentUser ? null : <NavLink className="inactive" activeclassname="active" to="/login">Login</NavLink>}
             <br></br>
             <NavLink className="inactive" activeclassname="active" to="/runners">Runners</NavLink>
             <br></br>
             <NavLink className="inactive" activeclassname="active" to="/coaches">Coaches</NavLink>
             <br></br>
             <NavLink className="inactive" activeclassname="active" to="/events">Events</NavLink>
+            <br></br>
+            { currentUser ? <button className="bttn" onClick={handleLogOut}>Logout</button> : null }
         </nav>
     )
 }
