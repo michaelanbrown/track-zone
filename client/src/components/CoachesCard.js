10a6b1ee0aae89d0e8ebbc52beb00b4fda15dd07
Michaela Brown <m_brownbrown@yahoo.com>
2023-04-21 18:40:43 -0700

    building out coachescard, all information is displaying

--- client/src/components/CoachesCard.js
+++ client/src/components/CoachesCard.js
@@ -1,12 +1,26 @@
 import React, { useContext } from 'react';
 import './App.css';
 import { UserContext } from '../context/Runner';
+import { Link, Routes, Route } from 'react-router-dom';
+import CoachesShow from './CoachesShow';
 
 function CoachesCard({ coach, coaches, setCoaches }) {
     const { currentUser, setCurrentUser } = useContext(UserContext);
 
         return (
-            <div>Coach</div>
+            <div>
+            <br/>
+            {coach.age ? <img className = "RunnerCardImg" src={coach.photo} alt={coach.full_name} width="40%" height="40%"/> : null}
+            {coach.age ? <p>Name: {coach.full_name}</p> : null}
+            {coach.age ? <p>Age: {coach.age}</p> : null}
+            {currentUser.admin ? <><Link to={`${coach.id}`}>View Details</Link>
+              <Routes>
+                <Route path={`coaches/${coach.id}`} element={<CoachesShow/>}/>
+              </Routes></> : null}
+            <br/>
+            <br/>
+            <br/>
+        </div>
     )
 }
 
