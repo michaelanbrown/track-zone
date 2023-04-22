9dfba9974428216b8ea07fc410ce3f714427d1cb
Michaela Brown <m_brownbrown@yahoo.com>
2023-04-21 18:01:28 -0700

    added coach presence validation

--- app/models/runner.rb
+++ app/models/runner.rb
@@ -6,6 +6,7 @@ class Runner < ApplicationRecord
   validates :age, numericality: { greater_than_or_equal_to: 13 }
   validates :event_id, presence: true
   validates :username, presence: true, uniqueness: true
+  validates :coach_id, presence: true
 
   has_secure_password
 end
