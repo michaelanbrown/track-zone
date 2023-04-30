5be9d341f30fee3ab467f9a9cf52e96725eca861
Michaela Brown <m_brownbrown@yahoo.com>
2023-04-29 17:42:39 -0700

    removed uniqueness for coaches because some people can have same name

--- app/models/coach.rb
+++ app/models/coach.rb
@@ -2,6 +2,6 @@ class Coach < ApplicationRecord
     has_many :runners
     has_many :events, through: :runners
 
-    validates :full_name, presence: true, uniqueness: true
+    validates :full_name, presence: true
     validates :age, numericality: { greater_than_or_equal_to: 18 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,7 @@
 c1 = Coach.create(full_name: "Lance Cortez", age: 44, photo: "https://lrbears.com/images/2017/6/28/FrontPageTRACK47.jpg?width=1884&quality=80&format=jpg")
 c2 = Coach.create(full_name: "Tiffany Hobbs", age: 28, photo: "https://ca-times.brightspotcdn.com/dims4/default/634e22d/2147483647/strip/true/crop/1024x576+0+0/resize/1200x675!/format/webp/quality/80/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2Fa2%2F8e%2Ff914ab1078e5c7f842c9360968cf%2Fla-1521760377-bxelrypq1v-snap-image")
 c3 = Coach.create(full_name: "Rodney Hale", age: 54, photo: "https://news.virginia.edu/sites/default/files/article_image/vic_lananna_header_3-2.jpg")
-c4 = Coach.create(full_name: "New Runner - Pick a Coach", age: 18, photo: nil)
 
-e1 = Event.create(name: "New Runner", distance: "1", unit_of_measurement: "km")
 e2 = Event.create(name: "Boston Marathon", distance: "26.2", unit_of_measurement: "mi")
 e3 = Event.create(name: "Run to Feed the Hungry", distance: "5", unit_of_measurement: "km")
 e4 = Event.create(name: "Run to Feed the Hungry", distance: "10", unit_of_measurement: "km")
