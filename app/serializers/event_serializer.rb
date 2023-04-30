5c25bd27a522ca64c3425c50b54d11e460c63dc2
Michaela Brown <m_brownbrown@yahoo.com>
2023-04-29 18:12:36 -0700

    added RunnersCoachSerializer to events serializer

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,5 +1,5 @@
 class EventSerializer < ActiveModel::Serializer
   attributes :id, :name, :distance, :unit_of_measurement
-  has_many :coaches
+  has_many :coaches, serializer: RunnersCoachSerializer
 
 end
